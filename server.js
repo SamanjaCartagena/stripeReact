4a45fdebb3789b06a84ed7305f67ad1ea8d7422b
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-07 23:11:11 -0500

    project finished stripe added

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const req = require('express/lib/request')
 const app = express()
 const cors = require('cors')
 app.use(cors({
-    origin:'http"//localhost:5500'
+    origin:'http://127.0.0.1:5500'
 }))
 
 app.use (express.json())
