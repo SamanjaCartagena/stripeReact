2acaf695c3141a4ddf531dfb1c3d712f043d945b
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-07 22:37:49 -0500

    stripe key added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
 const express = require('express')
-const app = express
+const req = require('express/lib/request')
+const app = express()
 
 
-app.request(express.json())
+app.use (express.json())
+app.use(express.static('public'))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
@@ -12,4 +14,33 @@ const storeItems= new Map([
     [1,{priceInCents:10000, name:'Learn React Today '}],
     [2,{priceInCents:20000, name:'Learn CSS Today'}]
 ])
+
+app.post('/create-checkout-session',async (req, res)=>{
+    try{
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types :['card'],
+            mode:'payment',
+            line_items: req.body.items.map(item => {
+               const storeItem = storeItems.get(item.id)
+               return {
+                   price_data :{
+                       currency:'usd', 
+                       product_data:{
+                        name:storeItem.name
+                       },
+                        unit_amount: storeItem.priceInCents
+                   },
+                   quantity:item.quantity
+               }
+            }),
+            success_url:`${process.env.SERVER_URL}/success.html`,
+            cancel_url:`${process.env.SERVER_URL}/cancel.html`
+            
+        })
+         res.json({url:session.url})
+    }catch(e){
+         res.status(500).json({error:e.message})
+    }
+    res.json({url: 'Hi'})
+})
 app.listen(3000)
